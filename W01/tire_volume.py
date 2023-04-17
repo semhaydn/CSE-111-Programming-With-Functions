60b5a86c7665d7cee847e988096673a594c66ca8
semhaydn <semhaydn@gmail.com>
2023-04-17 17:48:38 +0300

    initial commit

--- W01/tire_volume.py
+++ W01/tire_volume.py
@@ -5,30 +5,36 @@
 # Semih Aydin
 
 # import the math module
+# import the math module
 import math 
+
+# define constants for maximum allowed values
+MAX_WIDTH = 999
+MAX_ASPECT_RATIO = 999
+MAX_DIAMETER = 999
 
 # print a description and welcome
 print("Welcome to tire volume calculator")
 print("This program computes and outputs the volume of a tire")
 
-# get the with, aspetct ratio of the tire and the diameter of  the wheel
+# get the width, aspect ratio of the tire and the diameter of the wheel
 while True:
     try:
-        width = float(input("Enter the width of the tire in mm (ex 205): "))
-        if width < 0 or width > 999:
+        tire_width = float(input("Enter the width of the tire in mm (ex 205): "))
+        if tire_width < 0 or tire_width > MAX_WIDTH:
             raise ValueError
-        aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
-        if aspect_ratio < 0 or aspect_ratio > 999:
+        tire_aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
+        if tire_aspect_ratio < 0 or tire_aspect_ratio > MAX_ASPECT_RATIO:
             raise ValueError
-        diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
-        if diameter < 0 or diameter > 999:
+        wheel_diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
+        if wheel_diameter < 0 or wheel_diameter > MAX_DIAMETER:
             raise ValueError
         
         # compute the volume of the tire
-        volume = (((math.pi*width**2)*aspect_ratio)*(width*aspect_ratio + (2540*diameter)))/10000000000
+        volume = (((math.pi * tire_width ** 2) * tire_aspect_ratio) * (tire_width * tire_aspect_ratio + (2540 * wheel_diameter))) / 10000000000
         
         # print the volume of the tire for the user to see
         print(f"\nThe approximate volume is {volume:.2f} liters")
         break
     except ValueError:
-        print("Invalid input. Please enter a valid integer. (values must be between 0 and 999)")
+        print("Invalid input. Please enter a valid number. (values must be between 0 and 999)")
