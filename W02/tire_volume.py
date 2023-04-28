193e55afa537ad5668fb8f23f368adaf8eb72433
semhaydn <semhaydn@gmail.com>
2023-04-28 16:28:10 +0300

    initiial commit

--- W02/tire_volume.py
+++ W02/tire_volume.py
@@ -5,7 +5,6 @@
 # Semih Aydin
 
 # import the math module
-# import the math module
 import math 
 
 # define constants for maximum allowed values
@@ -17,27 +16,43 @@ MAX_DIAMETER = 999
 print("Welcome to tire volume calculator")
 print("This program computes and outputs the volume of a tire")
 
-# get the width, aspect ratio of the tire and the diameter of the wheel
+# define some constants for the maximum values of tire width, aspect ratio, and wheel diameter
+MAX_WIDTH = 999
+MAX_ASPECT_RATIO = 999
+MAX_DIAMETER = 999
+
+# get the tire width, aspect ratio, and wheel diameter from the user
 while True:
     try:
         tire_width = float(input("\nEnter the width of the tire in mm (ex 205): "))
+        # check if the tire width is within the allowable range
         if tire_width < 0 or tire_width > MAX_WIDTH:
+            # raise a ValueError if the tire width is not within the allowable range
             raise ValueError
         tire_aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
+        # check if the tire aspect ratio is within the allowable range
         if tire_aspect_ratio < 0 or tire_aspect_ratio > MAX_ASPECT_RATIO:
+            # raise a ValueError if the tire aspect ratio is not within the allowable range
             raise ValueError
         wheel_diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
+        # check if the wheel diameter is within the allowable range
         if wheel_diameter < 0 or wheel_diameter > MAX_DIAMETER:
+            # raise a ValueError if the wheel diameter is not within the allowable range
             raise ValueError
         
-        # compute the volume of the tire
+        # calculate the volume of the tire
         volume = (((math.pi * tire_width ** 2) * tire_aspect_ratio) * (tire_width * tire_aspect_ratio + (2540 * wheel_diameter))) / 10000000000
         
-        # print the volume of the tire for the user to see
+        # print the volume of the tire to the user
         print(f"\nThe approximate volume is {volume:.2f} liters")
+        
+        # exit the while loop since we have valid input from the user
         break
+    
     except ValueError:
+        # catch any invalid input and print an error message
         print("\nInvalid input. Please enter a valid number. (values must be between 0 and 999)")
+
 
 # Import the datetime class from the datetime
 # module so that it can be used in this program.
@@ -60,21 +75,45 @@ with open("volumes.txt","at") as volumes:
 
 while True:
     try:
-        buy = input("\nWould you like to buy the tire ? (yes or no) ").lower()
+        # ask the user if they want to buy the tire
+        buy = input("\nWould you like to buy the tire? (yes or no) ").lower()
+        
+        # check if the input is valid (either "yes" or "no")
         if buy != "yes" and buy != "no":
             raise ValueError("\nInvalid input, please enter 'yes' or 'no'")
+        
+        # if the user wants to buy the tire
         if buy == "yes": 
-            phone_number =(input("\nPlease provide us your number! our experienced sales assitants will call you, for the best offers! "))
+            # ask for their phone number
+            phone_number = input("\nPlease provide us your number! Our experienced sales assistants will call you for the best offers! ")
+            
+            # remove any non-digit characters from the phone number
             phone_number = ''.join(filter(str.isdigit, phone_number))
+            
+            # check if the phone number has the correct length
             if len(phone_number) != 10 :
                 raise ValueError("Your phone number should consist of 10 digits!! ")
-            print("\nThank you for choosing us! ")
+            
+            # print a thank you message
+            print("\nThank you for choosing us!")
+            
+            # append the phone number to the volumes.txt file
             with open('volumes.txt', 'a') as volumes:
                 print(phone_number, file=volumes)
+            
+            # exit the loop
             break
+        
+        # if the user does not want to buy the tire
         if buy == "no":
+            # print a goodbye message
             print("\nThank you! Have a great day!")
+            
+            # exit the loop
             break
+    
+    # handle any value errors (e.g., invalid input or phone number)
     except ValueError as e:
         print(e)
+
             
